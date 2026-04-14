6685b2b3e77811553d24cc5a2655945563df2533
Sissoko Mohammed <sissokomohammed10@gmail.com>
2026-04-14 14:32:49 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Workspace Connect</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="salle 1">
+        <div class= "salle 1">
+<h1>Réservez votre espace de travail idéal</h1>
+
+</body>
