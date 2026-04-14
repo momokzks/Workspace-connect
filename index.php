26fb6444ad609186d803bc6dc0f4f9003f4fac0a
Sissoko Mohammed <sissokomohammed10@gmail.com>
2026-04-14 14:59:32 +0200

    Ajout de la structure HTML et de la section hero

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Workspace Connect</title>
+    <meta name="viewport" content> 
+    <title>Nos Salles - WorkSpace Connect</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="salle 1">
-        <div class= "salle 1">
-<h1>Réservez votre espace de travail idéal</h1>
+    <header>
+        <h1>WorkSpace Connect</h1>
+        <nav>
+            <a href="index.php">Accueil</a> | 
+            <a href="salles.php">Nos salles</a> | 
+            <a href="reservation.php">Réserver</a>
+        </nav>
+    </header>
 
+    
+
+    <footer>
+        <p>Contact : contact@workspace-connect.fr | © 2026</p>
+    </footer>
 </body>
+</html>
